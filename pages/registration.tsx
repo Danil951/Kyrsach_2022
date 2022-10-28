c802e2964c112f3f00f091c65a5e993fdf840343
Danil951 <74674954+Danil951@users.noreply.github.com>
2022-10-29 00:04:34 +0300

    Add files via upload

--- pages/registration.tsx
+++ pages/registration.tsx
@@ -75,7 +75,7 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Регистрация
+            Регистрация228
           </Typography>
           <Box id="formRegistration" component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
